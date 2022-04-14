d68cf0f49e4ad5ef451828c6da66c5fa61083881
Francis Sebastian Quezon <francis.quezon@block64.com>
2022-04-14 15:30:40 +0800

    Added inputs to control generation of projection

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 (function() {
 const canvas = document.getElementById('myChart');
 const context = canvas.getContext('2d');
+const generateProjectionBtn = document.getElementById('generateProjectionBtn');
 
 const commissionFeeMult = 30;
-const initialBalance = 263.64;
-const pipSize = 60;
+//const initialBalance = 263.64;
+//const pipSize = 60;
 const initialDate = new Date(Date.now());
 const numOfGeneratedData = 100;
 
 window.addEventListener('resize', resizeCanvas, false);
+generateProjectionBtn.addEventListener('click', generateProjection);
+
 
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight-55;
+}
+
+function hideInputs() {
+    let pInput = document.getElementById('projectionInputs');
+    pInput.style.display = 'none';
+}
+function showProjection() {
+    let pChart = document.getElementById('projectionChartDiv');
+    let pTable = document.getElementById('projectionTableDiv');
+    pChart.style.display = '';
+    pTable.style.display = '';
 }
 
 function truncateTwoDecimal(num) {
@@ -64,7 +78,7 @@ function generateChartLabels(startDate, days) {
     return labels.map(x => x.toLocaleDateString('en-us', {day: 'numeric', month: 'short', year: 'numeric'}));
 }
 
-function populateTable() {
+function populateTable(initialBalance, pipSize) {
     const data = generateData(initialBalance, pipSize, numOfGeneratedData);
     const dates = generateChartLabels(initialDate, numOfGeneratedData);
     const table = document.getElementById('projectionTable');
@@ -95,42 +109,48 @@ function populateTable() {
     }
 }
 
-populateTable();
-resizeCanvas();
-const myChart = new Chart(context, {
-    type: 'line',
-    data: {
-        labels: generateChartLabels(initialDate, numOfGeneratedData),
-        datasets: [{
-            label: 'Projection',
-            data: generateChartData(initialBalance, pipSize, numOfGeneratedData),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+function generateProjection() {
+    const initialBalance = document.getElementById('initBalance').value;
+    const pipSize = document.getElementById('pipSize').value;
+    hideInputs();
+    showProjection();
+    populateTable(initialBalance, pipSize);
+    resizeCanvas();
+    const myChart = new Chart(context, {
+        type: 'line',
+        data: {
+            labels: generateChartLabels(initialDate, numOfGeneratedData),
+            datasets: [{
+                label: 'Projection',
+                data: generateChartData(initialBalance, pipSize, numOfGeneratedData),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
+}
 
 }
 )();
